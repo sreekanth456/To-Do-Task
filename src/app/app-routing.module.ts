290a8d41a96240b7296471404471fd3d07d6c8a5
sreekanth <sreekanth.koye@gmail.com>
2019-12-17 12:42:27 +0530

    html and css part over

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'home',component:HomeComponent},
+  {path:'projects',component:ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
